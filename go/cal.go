be375dc6606d733ba4b09cd13de73643770c24d5
siliconx <siliconx@163.com>
2018-05-11 04:35:16 -0400

    add go

---
+++ go/cal.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+    "log"
+    "math"
+    "net/http"
+)
+
+var msg string
+func main() {
+    args_len := len(os.Args)
+    if args_len == 2 {  // is circle
+        r, err := strconv.ParseFloat(os.Args[1], 64)  // get radius
+
+        if err != nil {
+            msg = fmt.Sprintf("Args value ERROR")
+            log.Fatal(err)
+        } else {
+            perimeter := 2 * math.Pi * r
+            area := math.Pi * math.Pow(r, 2)
+            msg = fmt.Sprintf("This is a circle\nr = %.3f\nperimeter = %.3f\narea = %.3f", r, perimeter, area)
+        }
+    } else if args_len == 3 {  // is rectangle
+        a, err := strconv.ParseFloat(os.Args[1], 64)
+        b, err := strconv.ParseFloat(os.Args[2], 64)
+
+        if err != nil {
+            msg = fmt.Sprintln("Args value ERROR")
+            log.Fatal(err)
+        } else {
+            perimeter := 2 * (a + b)
+            area := a * b
+            msg = fmt.Sprintf("This is a rectangle\na = %.3f\nb = %.3f\nperimeter = %.3f\narea = %.3f", a, b, perimeter, area)
+        }
+    } else {
+        msg = fmt.Sprintf("Args ERROR")
+    }
+
+    fmt.Println(msg)
+    http.HandleFunc("/", http_server)
+    http.ListenAndServe(":8080", nil)
+}
+
+func http_server(response http.ResponseWriter, request *http.Request) {
+    // fmt.Println(msg)
+    fmt.Fprintf(response, msg)
+}
---
+++ lab-report/report5.md
@@ -0,0 +1,72 @@
+#南昌大学实验报告
+
+姓名：<u>___谢志彬____</u>
+
+学号：<u>__6103115112_</u>
+
+邮箱地址：_________siliconx@163.com_____
+
+专业班级：_________<u>计算机科学与技术153</u>_____
+
+实验日期：<u>___2018/05/7   ___</u>
+
+课程名称：________<u>Linux程序设计实验</u>____
+
+##实验项目名称
+
+## Lab 5 -- Build it and Share it
+
+<h3 style="color: red">!!!由于openstack过于庞大，在我的笔记本电脑上无法成功部署服务，所以我把openstack换成了更加轻量级的服务 -- Docker</h3>
+
+
+
+##实验目的
+
+#####	1.理解laaS架构的机制
+
+##### 2.尝试部署一个工程服务
+
+##### 3.理解构建一个可视化web服务的过程
+
+
+
+##实验基础
+
+##### 1. Docker
+
+##### 2. Linux
+
+
+
+##实验步骤
+
+##### 1.安装docker
+
+> 使用Docker官方提供的脚本进行安装
+>
+> ```$ curl -fsSL get.docker.com -o get-docker.sh```
+>
+> ```$ sudo sh get-docker.sh --mirror Aliyun```
+>
+> ![](/home/siliconx/Pictures/Screenshot-from-2018-05-11-04-17-39.png)
+
+##### 2.启动Docker CE
+
+> ```$ sudo systemctl enable docker```
+>
+> ```$ sudo systemctl start docker```
+>
+> ![](/home/siliconx/Pictures/Screenshot-from-2018-05-11-04-20-07.png)
+
+##实验数据或结果
+
+
+
+##实验思考
+
+
+
+##参考资料
+
+
+
